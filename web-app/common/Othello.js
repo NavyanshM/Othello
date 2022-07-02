ef41c68742bd22ebabb0bc00b84752bf8f6af17a
Navyansh Malhotra <malhotra15@hotmail.com>
2022-07-02 22:42:18 +0100

    JS docs added

--- web-app/common/Othello.js
+++ web-app/common/Othello.js
@@ -5,28 +5,6 @@
  * @version v1.0
  */
 
-/**
- * @memberof Stats4
- * @typedef {Object} Statistics
- * @property {number} elo The Elo score of the player.
- * {@link https://en.wikipedia.org/wiki/Elo_rating_system}
- * @property {number} player_1_wins How many times the player
- * has won when playing first.
- * @property {number} player_1_losses How many times the player
- *     has lost when playing first.
- * @property {number} player_1_draws How many times the player has
- *     tied when playing first.
- * @property {number} player_2_wins How many times the player has
- *     won when playing second.
- * @property {number} player_2_losses How many times the player has
- *     lost when playing second.
- * @property {number} player_2_draws How many times the player has
- *     tied when playing second.
- * @property {number} current_streak The number of games the player has won
- *     since last losing (or ever if the player has never lost).
- * @property {number} longest_streak The most consecutive games won.
- */
-
 import R from "../common/ramda.js";
 const Othello = Object.create(null);
 
